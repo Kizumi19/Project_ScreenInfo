597fa03aa58c776f7a267c7db8e82817367e0a3a
Kizumi19 <juliakbgamer12@gmail.com>
2023-09-29 13:57:56 +0200

    Form, Inserts, and first part of constructors

---
+++ DocumentRoot/PHP/doctor.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doctor info</title>
+</head>
+<body>
+<?php
+class Doctor{
+
+    private $id;
+    private $name;
+    private $surname;
+    private $specialty_id;
+    private $location_id;
+    private $hidden;
+
+    
+}
+?>
+
+</body>
+</html>
