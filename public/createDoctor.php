032dc03db3dd6bac8c4e1864f297a7567ba91ea9
Kizumi19 <juliakbgamer12@gmail.com>
2023-10-05 14:57:13 +0200

    Checkpoint part 2

--- public/createDoctor.php
+++ public/createDoctor.php
@@ -2,19 +2,22 @@
 
 include(__DIR__ . '/connect.php');  
 
-$id = 9;
 $name = $_POST['Nom'];
 $surname = $_POST['Cognom'];
 $specialty_id = $_POST['id_especialitat'];
 $location_id = $_POST['id_localitzacio'];
 $hidden = 0;
 
+if ($location_id = 1){
+    
+}
+
 // Sentència preparada
 $stmt = $conn->prepare("INSERT INTO doctors (id, name, surname, specialty_id, location_id, hidden) VALUES (?, ?, ?, ?, ?, ?)");
 if ($stmt === false) {
     die("Error: " . $conn->error);
 }
-$stmt->bind_param("issiii", $id, $name, $surname, $specialty_id, $location_id, $hidden);  // Añadido un "i" extra para $hidden
+$stmt->bind_param("issiii", $id, $name, $surname, $specialty_id, $location_id, $hidden);  
 
 $result = $stmt->execute();
 
