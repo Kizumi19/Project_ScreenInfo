84feb8687ad0620925c1044bea7ee76ce9e54a8f
Kizumi19 <juliakbgamer12@gmail.com>
2023-10-05 12:44:03 +0200

    Added AUTO_INCREMENT in the id's of each table

--- Screen_Information.sql
+++ Screen_Information.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `schedules` (
-  `id` integer PRIMARY KEY,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `doctor_id` int,
   `shift` ENUM ('day', 'afternoon'),
   `hidden` boolean
 );
 
 CREATE TABLE `users` (
-  `id` integer PRIMARY KEY,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `username` varchar(255),
   `role` ENUM ('admin', 'viewer'),
   `created_at` timestamp,
@@ -14,7 +14,7 @@ CREATE TABLE `users` (
 );
 
 CREATE TABLE `locations` (
-  `id` integer PRIMARY KEY,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `floor` ENUM ('0', '1', '2', '3', '4'),
   `room` int,
   `created_at` timestamp,
@@ -22,24 +22,22 @@ CREATE TABLE `locations` (
 );
 
 CREATE TABLE `doctors` (
-  `id` integer PRIMARY KEY,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `name` varchar(255),
   `surname` varchar(255),
-  `specialty_id` integer,
-  `location_id` integer,
+  `specialty_id` int,
+  `location_id` int,
   `created_at` timestamp,
   `hidden` boolean
 );
 
 CREATE TABLE `specialties` (
-  `id` integer PRIMARY KEY,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `type_specialty` varchar(255),
   `created_at` timestamp,
   `hidden` boolean
 );
 
 ALTER TABLE `schedules` ADD FOREIGN KEY (`doctor_id`) REFERENCES `doctors` (`id`);
-
 ALTER TABLE `doctors` ADD FOREIGN KEY (`specialty_id`) REFERENCES `specialties` (`id`);
-
 ALTER TABLE `doctors` ADD FOREIGN KEY (`location_id`) REFERENCES `locations` (`id`);
