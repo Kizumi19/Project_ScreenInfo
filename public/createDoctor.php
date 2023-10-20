beb0e9446bd6f79e26ef47d37c3d376a302e9321
Kizumi19 <juliakbgamer12@gmail.com>
2023-10-20 09:33:19 +0200

    Update form Front-End

--- public/createDoctor.php
+++ public/createDoctor.php
@@ -11,7 +11,35 @@ $type_specialty = $_POST['TipusEspecialitat'];
 //Info a enllaçar
 $floor = $_POST['Planta'];
 $room = $_POST['Consulta'];
+$shift = $_POST['Torn'];
+$day = $_POST['Dia'];
+
 $hidden = 0;
+
+// Passar d'idioma de català al de la base de dades
+if ($shift == 'Matí') {
+ $shift = 'morning';   
+} else if($shift == 'Tarde')  {
+    $shift = 'afternoon';
+}
+
+// Passar d'idioma de català al de la base de dades
+if ($day == 'Dilluns') {
+    $day = 'monday';   
+   } else if($day == 'Dimarts')  {
+       $day = 'tueday';
+   } else if($day == 'Dimecres') {
+    $day = 'wednesday';
+   }
+else if($day == 'Dijous') {
+    $day = 'tuesday';
+} else if($day == 'Divendres') {
+    $day = 'friday';
+} else if($day == 'Dissabte') {
+    $day = 'saturday';
+} else if($day == 'Diumenge') {
+    $day = 'sunday';
+}
 
 
 // Sentència preparada -- Info bàsoca
